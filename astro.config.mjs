6e90f960cc8001dd6ef9ffa0203314cb15d87f3e
jont <60321773+jmt1423@users.noreply.github.com>
2025-05-31 12:01:53 +0100

    refactor: changed files to .mdx and added a contribution page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,38 +3,39 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Form Forge Docs",
-      social: [
-        {
-          icon: "github",
-          label: "GitHub",
-          href: "https://github.com/jmt1423/vue-form-forge",
-        },
-      ],
-      sidebar: [
-        {
-          label: "Core",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Installation", slug: "core/installation" },
-            { label: "Theming", slug: "core/theming" },
-            { label: "AI Assistant", slug: "core/ai-assistant" },
-            { label: "UI Components", slug: "core/ui-components" },
-            { label: "Default Items and Pro Inputs", slug: "core/pro-inputs"}
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-      customCss: ["./src/styles/global.css"],
-    }),
-  ],
+  site: "https://vueformforge.com/",
+  integrations: [starlight({
+    title: "Form Forge Docs",
+    social: [
+      {
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/jmt1423/vue-form-forge",
+      },
+    ],
+    sidebar: [
+      {
+        label: "Core",
+        items: [
+          // Each item here is one entry in the navigation menu.
+          { label: "Installation", slug: "core/installation" },
+          { label: "Theming", slug: "core/theming" },
+          { label: "AI Assistant", slug: "core/ai-assistant" },
+          { label: "UI Components", slug: "core/ui-components" },
+          { label: "Default Items and Pro Inputs", slug: "core/pro-inputs"}
+        ],
+      },
+      {
+        label: "Reference",
+        autogenerate: { directory: "reference" },
+      },
+    ],
+    customCss: ["./src/styles/global.css"],
+  }), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
